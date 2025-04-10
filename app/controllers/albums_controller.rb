cc1eb23a919679a839877b5b155a31e2ddc7db6f
sapopo888 <sapopo0813@yahoo.co.jp>
2025-04-11 04:14:09 +0900

    add:albumコントローラー作成・記述、ルーティング修正

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,43 @@
+class AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      flash[:notice] = 'アルバム作成に成功しました'
+      redirect_to @album
+    else
+      flash.now[:alert] = 'アルバム作成に失敗しました'
+      render :new
+    end
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update(album_params)
+      flash[:notice] = 'アルバムを更新しました'
+      redirect_to @album
+    else
+      flash.now[:danger] = 'アルバムの更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :description)
+  end
+end
