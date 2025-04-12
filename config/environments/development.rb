c6f40f52c418715e9b15906d293413e994ab60d0
sapopo888 <sapopo0813@yahoo.co.jp>
2025-04-12 14:28:55 +0900

    add:デバックようgem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  # Docker環境においてbetter_errorsが反映されてない不具合への対処
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
